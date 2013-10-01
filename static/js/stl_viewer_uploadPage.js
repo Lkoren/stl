ccb5166f890779276fd96b8c86d69532f230fa5c
Liav <liav[dot]koren[at]gmail>
2013-10-01 15:29:46 -0400

    Local loading code cleaned up. Asynch isn't working correctly, load only completes for small files.

--- static/js/stl_viewer_uploadPage.js
+++ static/js/stl_viewer_uploadPage.js
@@ -5,31 +5,19 @@ $(document).ready(function() {
 		e.preventDefault()
 		var f = e.target.files[0]
 		var reader = new FileReader()
-
-
 		var mycanvas = document.getElementById('upload_canvas');
 		var viewer = new JSC3D.Viewer(mycanvas);
 		var theScene = new JSC3D.Scene
-		//var stlpath = mycanvas.getAttribute('stlpath');
-		//viewer.setParameter('SceneUrl', f.name);
+
 	   	viewer.setParameter('InitRotationX', 20);
 		viewer.setParameter('InitRotationY', 20);
 		viewer.setParameter('InitRotationZ', 0);
 		viewer.setParameter('ModelColor', '#CAA618');
 		viewer.setParameter('BackgroundColor1', '#FFFFFF');
 		viewer.setParameter('BackgroundColor2', '#383840');
-		//viewer.setParameter('RenderMode', mycanvas.getAttribute('rmode'));
 		viewer.setParameter('RenderMode', "flat");
 
 		var stl_loader = new JSC3D.StlLoader()
-	  	/*
-	  	stl_loader.onload = function(scene) {
-	  		console.log("stl_loader onload")
-	    	viewer.replaceScene(scene)
-			viewer.init();
-			viewer.update();
-	  	}*/
-
 	  	var onModelLoaded = function(scene) {
 		    var meshes = scene.getChildren()
 		    theScene.addChild(meshes[0])
@@ -40,18 +28,12 @@ $(document).ready(function() {
 			return function(e) {
 				console.log("file data is: ", e.target.result)
 		    	var loader = new JSC3D.StlLoader
-		    	//loader.onload = onModelLoaded
 		    	stl_loader.parseStl(theScene, e.target.result)
 				var meshes = theScene.getChildren()
 			    theScene.addChild(meshes[0])
 		      	viewer.init()
 		      	viewer.replaceScene(theScene)
 		      	viewer.update()
-
-		    	/*viewer.replaceScene(scene)
-				viewer.init();
-				viewer.update();
-				stl_loader.parseStl(scene, e.target.result)*/
 			}
 		})(f)
 
@@ -63,22 +45,6 @@ $(document).ready(function() {
 		if (ext.toLowerCase() != "stl") {
 			alert("That doesn't appear to be an STL file.");
 		} else {
-
-
-			/*
-				data = {}
-				data.data = "test test"
-				data._xsrf = getCookie("_xsrf")
-				$.ajax({
-					type: "POST",
-					url: "preview",
-					data: data,
-					enctype: 'multipart/form-data',
-					//dataType: 'binary',
-					//mimeType: 'text/plain; charset=x-user-defined',
-					success: function() { preview(data) }
-				})
-			*/
 
 			}
 		}
