5e6ba48dc5c5a2d87f2e63c2465c8209a743b4d7
Liav <liav[dot]koren[at]gmail>
2013-10-01 10:59:49 -0400

    Clean up to stl_server files as per PEP8 + JSC3D now works in results page.
    Stl loading happens via the server.

--- static/js/admin.js
+++ static/js/admin.js
@@ -5,7 +5,7 @@ var printer_data
 new structure:
 printers = {
     "foo": [{"option_name":"option_cost"}, ...],
-    "bar": [{"option_name":"option_cost"}, ...], 
+    "bar": [{"option_name":"option_cost"}, ...],
     .
     .
     .
@@ -35,7 +35,7 @@ var printer_exists = function(name) {
 
 
 ////Define the printers available: eg, Makerbot1, Makerbot2, Form1, Zcorpt
-function make_name_field(id) { //the field which names each type of printer: Makerbot1, Makerbot2, Form1, Form2, Zcorp, etc..    
+function make_name_field(id) { //the field which names each type of printer: Makerbot1, Makerbot2, Form1, Form2, Zcorp, etc..
     console.log("hi!")
     var str = "<li><input type='text' id='printer_"+id+"' maxlength='80'/> \
     <input class='define_options' onclick='show_printer_option("+id+")' type='button' value='Define options'/> \
@@ -44,7 +44,7 @@ function make_name_field(id) { //the field which names each type of printer: Mak
     str = "#p" + id + "_options"
     var options = $(str)
 }
-function remove_name_field() {        
+function remove_name_field() {
     if (get_list_size("#printers") > 1) {
         $("#printers").find("li:last").remove()
     }
@@ -52,22 +52,22 @@ function remove_name_field() {
 function remove_printer_row(row){
     var printer_name = $("#printers li:nth-child("+row+")").find("input[type=text]").val()
     if (printer_name in printer_data) remove_printer(printer_name)
-    $("#printers li:nth-child("+row+")").remove()   
+    $("#printers li:nth-child("+row+")").remove()
     $("#enum_printers").val($("#enum_printers").val()-1)
 }
 ////User interaction with the Printer Options area, eg, defining services offered by Makerbot1: low, med, hi quality @ x,y,z price.
 function enable_controls(enabled) {
-    var controls = [$("#enum_printers")[0], $("#submit_button")[0]]    
+    var controls = [$("#enum_printers")[0], $("#submit_button")[0]]
     build_control_list(".define_options")
     build_control_list("#printers input[type='text']")
-    function build_control_list(target) { 
+    function build_control_list(target) {
         $(target).each(function() { controls.push($(this)[0])
         })
     }
     function set_all_controls(enabled) {
         controls.forEach(function(node) {
            // !(enabled) ? node.setAttribute('disabled', true) : node.removeAttribute('disabled')
-           enabled ? node.removeAttribute('disabled') : node.setAttribute('disabled', true) 
+           enabled ? node.removeAttribute('disabled') : node.setAttribute('disabled', true)
         })
     }
     set_all_controls(enabled)
@@ -78,26 +78,26 @@ var show_printer_option = function(id, name, cost)  { //ToDo: refactor. Remove c
 
     name === undefined ? option_name = '' : option_name = name
     cost === undefined ? option_cost = '0.00' : option_cost = cost
-    if (id) {        
-        var n = "#printer_" + id  
-        printer_name = $(n).val()          
+    if (id) {
+        var n = "#printer_" + id
+        printer_name = $(n).val()
         $("#printer_options").prepend("<div class='printer_name'>")
         $(".printer_name").text("Options for: " + printer_name)
     }
-    id = id || ($("#printer_options").find("li").size() + 1) //can be called externally or recursively. This is for recursive calls. 
+    id = id || ($("#printer_options").find("li").size() + 1) //can be called externally or recursively. This is for recursive calls.
     enable_controls(false)
-    var target = $("#printer_options").find("ul")    
+    var target = $("#printer_options").find("ul")
     var insert_string = "<li><label>Service name: <input type='text' value = '"+option_name+"' id='option_"+id+"_name' required></label> \
     <input type='number' id='option_"+id+"_cost' pattern='[0-9]+\.[0-9]+' value='"+option_cost+"' min='0.0' step='0.01'><label>Cost per CC</label>"
     insert_string += "<input type='button' value='Remove option' onclick='remove_printer_option('"+id+"')'>"
     if (get_list_size("#printer_options") < 1) {
-        show_option_header_buttons($("#printer_options p"), printer_name)   
-    }    
-    target.append(insert_string)    
+        show_option_header_buttons($("#printer_options p"), printer_name)
+    }
+    target.append(insert_string)
     populate_printer_options(printer_name)
 }
 
-function show_option_header_buttons(target, name) {    
+function show_option_header_buttons(target, name) {
     var insert_string = "<input type='button' value='Add option' onclick='show_printer_option()'>"
     insert_string += "<input type='button' id='cache_settings_button' onclick='cache_printer_options()'> </li>"
     target.append("<div id = 'option_buttons'>")
@@ -114,17 +114,17 @@ function populate_printer_options(printer) {
         var first_item = $("#printer_options li")
         var printer_options_array = printer_data[printer]
         for (var i = 0; i < printer_options_array.length; i++) {
-        var option = printer_options_array[i]   
+        var option = printer_options_array[i]
         for (option_name in option) {
             if (i === 0) { //the first row is created by the "define options button. Insert vals seperately."
                 var target = $("#printer_options ul").find("li:first")
                 target.find("input[type=text]").val(option_name)
                 target.find("input[type=number]").val(option[option_name])
-                } else { 
+                } else {
                     show_printer_option(null, option_name, option[option_name])
                 }
             }
-        }                
+        }
     }
 }
 
@@ -153,7 +153,7 @@ function cache_printer_options(){  //ToDo: refactor this + document init code to
     set_printer(name, options)
     clear_printer_options()
     remove_option_header_buttons()
-  
+
 }
 
 ////Utility functions
@@ -171,11 +171,11 @@ function process_form() {
 
 }
 function update_form() {
-    var printers = $("#enum_printers").val()        
-    if (printers != get_list_size("#printers")) {    
+    var printers = $("#enum_printers").val()
+    if (printers != get_list_size("#printers")) {
         printers > $("#printers").find("li").size() ? make_name_field(printers) : remove_name_field()
     }
-    prev_printers = printers        
+    prev_printers = printers
 }
 function get_num(id){ //ToDo: only called once, remove.
     return $(id).val()
@@ -211,14 +211,14 @@ update_form()
 new structure:
 printers = {
     "foo": [{"option_name":"option_cost"}, ...],
-    "bar": [{"option_name":"option_cost"}, ...], 
+    "bar": [{"option_name":"option_cost"}, ...],
     .
     .
     .
 }
 printers = {
     "foo": [{"high quality":24.63}, {"med quality":2.63}, {"low quality":0.63}],
-    "bar": [{"high quality":4.63}, {"med quality":1.63}, {"low quality": 0.32}], 
+    "bar": [{"high quality":4.63}, {"med quality":1.63}, {"low quality": 0.32}],
     "baz": [{"quux quality": 246.3}, {"zzz+++ quality": 35.2}]
 }
 
@@ -227,5 +227,5 @@ printers = {
     "printers": [ {"makerbot": [{"draft quality": 2.00}, {"medium quality": 4.00}, {"high quality": 6.00}]},
       {"form1": [{"draft quality": 5.00}, {"high quality": "10.00"}]}
     ]
-} 
+}
 */
--- static/js/stl_viewer_ajax.js
+++ static/js/stl_viewer_ajax.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {	
+$(document).ready(function() {
 	/*
 	jQuery.ajaxSetup({
 	    accepts: {
@@ -18,26 +18,43 @@ $(document).ready(function() {
 
 	var handle_file_select = function(e) {
 		e.stopPropagation()
-		e.preventDefault()		
-		f = e.target.files[0]			
-		ext = f.name.split(".")[1]
+		e.preventDefault()
+		var f = e.target.files[0]
+		console.log("file : ", f)
+		var ext = f.name.split(".")[1]
+		var data
 		if (ext.toLowerCase() != "stl") {
 			alert("That doesn't appear to be an STL file.");
-		
 		} else {
+				data = {}
+				data.data = "test test"
+				data._xsrf = getCookie("_xsrf")
 				$.ajax({
 					type: "POST",
-					url: "/preview",
-					data: f,
-					dataType: 'binary',
-					mimeType: 'text/plain; charset=x-user-defined',
-					success: preview
+					url: "preview",
+					data: data,
+					enctype: 'multipart/form-data',
+					//dataType: 'binary',
+					//mimeType: 'text/plain; charset=x-user-defined',
+					success: function() { preview(data) }
 				})
 			}
 		}
+
+		function preview(data) {
+			console.log("success: ", data)
+		}
+
+		function getCookie(name) {
+		    var c = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+		    return c ? c[1] : undefined;
+		}
+
+	/*
 	if (window.File) {
 		document.getElementById('file_select').addEventListener('change', handle_file_select, false)
 	}
+	*/
 })
 
 
