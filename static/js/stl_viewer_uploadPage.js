ccd17c4b6afcc9d325c22430f98f5f7536a4dd9d
Liav <liav[dot]koren[at]gmail>
2013-10-01 16:02:14 -0400

    Asynch loading still kind of messed up.

--- static/js/stl_viewer_uploadPage.js
+++ static/js/stl_viewer_uploadPage.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
 	var handle_file_select = function(e) {
-		e.stopPropagation()
-		e.preventDefault()
+		//e.stopPropagation()
+		//e.preventDefault()
 		var f = e.target.files[0]
 		var reader = new FileReader()
 		var mycanvas = document.getElementById('upload_canvas');
@@ -18,19 +18,10 @@ $(document).ready(function() {
 		viewer.setParameter('RenderMode', "flat");
 
 		var stl_loader = new JSC3D.StlLoader()
-	  	var onModelLoaded = function(scene) {
-		    var meshes = scene.getChildren()
-		    theScene.addChild(meshes[0])
-	      	viewer.replaceScene(theScene)
-		}
-
 		reader.onload = (function(file) {
 			return function(e) {
 				console.log("file data is: ", e.target.result)
-		    	var loader = new JSC3D.StlLoader
 		    	stl_loader.parseStl(theScene, e.target.result)
-				var meshes = theScene.getChildren()
-			    theScene.addChild(meshes[0])
 		      	viewer.init()
 		      	viewer.replaceScene(theScene)
 		      	viewer.update()
